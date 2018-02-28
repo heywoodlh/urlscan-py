d14c423b685556592ef835fe376a7894fb9262fb
Spencer Heywood <l.spencer.heywood@gmail.com>
2018-02-28 14:04:43 -0700

    set long_description to README.md

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+import io
 
 setup(name='urlscan-py',
       version='1.0',
@@ -17,3 +19,9 @@ setup(name='urlscan-py',
           'urllib3',
       ],
       zip_safe=False)
+
+here = os.path.dirname(__file__)
+
+readme_path = os.path.join(here, 'README.md')
+with io.open(readme_path, encoding='utf-8') as readme_file:
+    long_description = readme_file.read()
