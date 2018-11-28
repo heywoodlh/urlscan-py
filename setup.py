48eb69479aff158cc0c78aaecabff58c2d1a927a
Spencer Heywood <l.spencer.heywood@gmail.com>
2018-11-28 11:01:32 -0700

    updated readme and added error code return if retrieval fails

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='urlscan-py',
-      version='1.0.2.1',
+      version='1.0.3',
       description='urlscan.io API wrapper',
       long_description=readme(),
       url='https://github.com/heywoodlh/urlscan-py',
